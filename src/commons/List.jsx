915da4d6013b51e2fcb75b6b9825d1024d26a840
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2023-06-27 21:45:57 -0600

    Kevin View order filter state

---
+++ src/commons/List.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const List = ({ contents }) => {
+    if (!contents || contents.length === 0) return <p>No hay contenidos</p>
+    
+    const getStateLabel = (state) => {
+        if (state === "on_time") {
+            return "A tiempo";
+        } else if (state === "late") {
+            return "Sobre Tiempo";
+        } else if (state === "delayed") {
+            return "Demorado";
+        }
+    }
+
+    return (
+        <div>
+            <h4>Estos son los datos disponibles</h4>
+            <table className="table table-striped-columns">
+                <thead>
+                    <tr>
+                    <th style={{padding: "0 4em 0 4em"}}>Fecha</th>
+                    <th style={{padding: "0 4em 0 4em"}}>Estado</th>
+                    <th style={{padding: "0 4em 0 4em"}}>Plato</th>
+                    <th style={{padding: "0 4em 0 4em"}}>Cliente</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contents.map(({date, state, dish: {name}, client: {first_name}}) => (
+                    <tr>
+                        <td>{date}</td>
+                        <td>{getStateLabel(state)}</td>
+                        <td>{name}</td>
+                        <td>{first_name}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default List;
